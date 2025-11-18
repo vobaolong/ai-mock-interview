6f048902e42f3c92d52bf862eb0b9b003f9e743d
NHN <nhn@192.168.1.3>
2025-11-18 19:24:55 +0700

    update

--- src/App.tsx
+++ src/App.tsx
@@ -1,50 +1,116 @@
+import { Suspense, lazy } from 'react'
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
-import { CreateEditPage } from './routes/create-edit-page'
-import { MockLoadPage } from './routes/mock-load-page'
-import { Feedback } from './routes/feedback'
-import { MockInterviewPage } from './routes/mock-interview-page'
-import { Dashboard } from './routes/dashboard'
+
 import ProtectedRoute from './layouts/protected-route'
-import MainLayout from './layouts/main-layout'
-import { Generate } from './views/generate'
-import { PublicLayout } from './layouts/public-layout'
-import { ContactUsPage, HomePage, SignInPage, SignUpPage } from './routes'
+import { LoaderPage } from './views/loader-page'
+
+const PublicLayout = lazy(() =>
+  import('./layouts/public-layout').then((module) => ({
+    default: module.PublicLayout
+  }))
+)
+
+const MainLayout = lazy(() =>
+  import('./layouts/main-layout').then((module) => ({
+    default: module.default
+  }))
+)
+
+const HomePage = lazy(() =>
+  import('./routes/home-page').then((module) => ({
+    default: module.HomePage
+  }))
+)
+
+const SignInPage = lazy(() =>
+  import('./routes/sign-in').then((module) => ({
+    default: module.SignInPage
+  }))
+)
+
+const SignUpPage = lazy(() =>
+  import('./routes/sign-up').then((module) => ({
+    default: module.SignUpPage
+  }))
+)
+
+const ContactUsPage = lazy(() =>
+  import('./routes/contact').then((module) => ({
+    default: module.ContactUsPage
+  }))
+)
+
+const Generate = lazy(() =>
+  import('./views/generate').then((module) => ({
+    default: module.Generate
+  }))
+)
+
+const Dashboard = lazy(() =>
+  import('./routes/dashboard').then((module) => ({
+    default: module.Dashboard
+  }))
+)
+
+const CreateEditPage = lazy(() =>
+  import('./routes/create-edit-page').then((module) => ({
+    default: module.CreateEditPage
+  }))
+)
+
+const MockLoadPage = lazy(() =>
+  import('./routes/mock-load-page').then((module) => ({
+    default: module.MockLoadPage
+  }))
+)
+
+const MockInterviewPage = lazy(() =>
+  import('./routes/mock-interview-page').then((module) => ({
+    default: module.MockInterviewPage
+  }))
+)
+
+const Feedback = lazy(() =>
+  import('./routes/feedback').then((module) => ({
+    default: module.Feedback
+  }))
+)
 
 export default function App() {
   return (
     <Router>
-      <Routes>
-        {/* public routes */}
+      <Suspense fallback={<LoaderPage />}>
+        <Routes>
+          {/* public routes */}
+          <Route path='/sign-in' element={<SignInPage />} />
+          <Route path='/sign-up' element={<SignUpPage />} />
+          <Route path='/contact' element={<ContactUsPage />} />
 
-        <Route path='/sign-in' element={<SignInPage />} />
-        <Route path='/sign-up' element={<SignUpPage />} />
-        <Route path='/contact' element={<ContactUsPage />} />
-
-        <Route element={<PublicLayout />}>
-          <Route path='/' element={<HomePage />} />
-        </Route>
-
-        {/* protected routes */}
-        <Route
-          element={
-            <ProtectedRoute>
-              <MainLayout />
-            </ProtectedRoute>
-          }
-        >
-          <Route path='/generate' element={<Generate />}>
-            <Route index element={<Dashboard />} />
-            {/* create route */}
-            <Route path=':interviewId' element={<CreateEditPage />} />
-            <Route path='interview/:interviewId' element={<MockLoadPage />} />
-            <Route
-              path='interview/:interviewId/start'
-              element={<MockInterviewPage />}
-            />
-            <Route path='feedback/:interviewId' element={<Feedback />} />
+          <Route element={<PublicLayout />}>
+            <Route path='/' element={<HomePage />} />
           </Route>
-        </Route>
-      </Routes>
+
+          {/* protected routes */}
+          <Route
+            element={
+              <ProtectedRoute>
+                <MainLayout />
+              </ProtectedRoute>
+            }
+          >
+            <Route path='/generate' element={<Generate />}>
+              <Route index element={<Dashboard />} />
+              <Route path=':interviewId' element={<CreateEditPage />} />
+              <Route path='interview/:interviewId' element={<MockLoadPage />} />
+              <Route
+                path='interview/:interviewId/start'
+                element={<MockInterviewPage />}
+              />
+              <Route path='feedback/:interviewId' element={<Feedback />} />
+            </Route>
+          </Route>
+        </Routes>
+      </Suspense>
     </Router>
   )
 }
